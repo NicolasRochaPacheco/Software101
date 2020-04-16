9d42f6f67d1ccee20e5b4c9cc55cf60caa3eee8e
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2020-04-16 16:11:55 -0500

    Non logical TP1. Deprecation coming soon

--- asm/TP1.asm
+++ asm/TP1.asm
@@ -1,8 +1,22 @@
+# File asm/tp1.asm "Test Program 1 (TP1)"
+# TODO Must include all arithmetic and logical
+# operations to be considered an approved test
+
+
 .global	_main
 
-_main:	addi x1, x0, 3
-		addi x2, x0, 2
-		add  x3, x1, x2
-		add  x4, x3, x3
-		sub  x5, x4, x1
-		and  x6, x1, x3
+_main:
+	addi x1, x0, 3
+	addi x2, x0, 2
+	add  x3, x1, x2
+	sub  x4, x1, x2
+	and  x5, x4, x2
+	andi x5, x5, 5
+	addi x6, x0, -3
+	slt  x7, x6, x1
+	sltu x8, x6, x1
+	addi x9, x0, 2
+	sll  x10, x9, x4
+	addi x11, x0, -8
+	sra  x12, x11, x4
+	add  x13, x12, x0
